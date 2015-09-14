d461f911cb6baf8214f364c31d06ee65f9c18473
gorobetssergey <momona@ukr.net>
2015-09-15 01:33:39 +0300

    Переход на ООП..соединение с БД

---
+++ classes/DB.php
@@ -0,0 +1,60 @@
+<?php
+class DB
+{
+    /* private $link = null;
+     public function __construct($host, $user, $pass, $db_name, $port=3306)
+     {
+         $this->link = mysqli_connect($host, $user, $pass, $db_name, $port);
+         if (mysqli_connect_errno())
+             die("Ошибка подключения (".mysqli_connect_errno()."): ".mysqli_connect_error());
+     }
+
+     public function query($query_string)
+     {
+         $result = mysqli_query($this->link, $query_string);
+         if (!$result)
+             die("Ошибка запроса (".mysqli_errno($this->link)."): ".mysqli_error($this->link));
+         return $result;
+     }
+
+     public function close()
+     {
+         if ($this->link) mysqli_close($this->link);
+     }
+ }
+
+ $db = new DB('localhost', 'root', 'qscvhuki', 'news');
+ $result = $db->query('SELECT * FROM news');
+
+ $ret=[];
+ foreach($result as $a)
+ {
+     foreach($a as $b) {
+         echo $b.'///';
+     }
+     echo '<br>';*/
+    protected $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=news', 'root', 'qscvhuki');
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo->exec('SET NAMES "utf8"');
+        } catch (PDOException $e) {
+            exit('Невозможно подключиться к серверу баз данных: ' . $e->getMessage());
+        }
+    }
+
+    public function query($sql)
+    {
+        $res= $this->pdo->query($sql);
+        $ret=[];
+        while($row=$res->fetch(PDO::FETCH_OBJ))
+        {
+            $ret[]=$row;
+        }
+        return $ret;
+    }
+}
+
--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
+require __DIR__.'/classes/DB.php';
 
-require_once __DIR__.'/func.php';
+$ob=new DB();
+$res=$ob->query('SELECT * FROM news');
+print_r($res);
 
-if(isset($_GET['News']) AND $_GET['News']=='All'){
-    require __DIR__.'/controllers/NewsAll.php';
-    $ob=new NewsAll('news');
-    NewsAll::All();
-}
-if((isset($_GET['News']) AND $_GET['News']=='One') AND (isset($_GET['Num'])))
-{
-    require __DIR__.'/controllers/NewsOne.php';
-    $ob=new NewsONE('News',$_GET['Num']);
-    NewsONE::ONE();
-}
